71a7d293ffaf2f99933f89c03b10126a60ef411e
hamai <hamai@hamais-MacBook-Pro.local>
2020-07-16 18:01:23 -0400

    more changes to mobile.

--- docs/app.js
+++ docs/app.js
@@ -69,7 +69,7 @@ const showMenu =(event) =>{
                 $('.close').css('display', 'none');
                 $burger.css('display', 'block');    
                 $('.inline-menu').css('display', 'block');
-                $('.mobile-menu-show').css('visibility', 'none');
+                $('.mobile-menu-show').css('opacity', '0');
                 $('.mobile-menu-show').css('margin-left','-100vh');
                 $('.title-name').css('display','block');
 
@@ -85,7 +85,7 @@ const showMenu =(event) =>{
         }else{
             $('.close').css('display', 'block');
             $('.inline-menu').css('display', 'none');
-            $('.mobile-menu-show').css('visibility', 'visible');
+            $('.mobile-menu-show').css('opacity', '1');
             $('.mobile-menu-show').css('margin-left','0');
             $('.title-name').css('display','none');
             $burger.css('display', 'none');
