8efd805ca0b091eb7e708fb8dc0d636bd7a5d9aa
hamai <hamai@hamais-MacBook-Pro.local>
2020-07-16 17:29:25 -0400

    mobile menu debugging

--- docs/app.js
+++ docs/app.js
@@ -70,6 +70,7 @@ const showMenu =(event) =>{
                 $burger.css('display', 'block');    
                 $('.inline-menu').css('display', 'block');
                 $('.mobile-menu-show').css('display', 'none');
+                $('.mobile-menu-back').css('display','none');
                 $('.mobile-menu-back').css('margin-left','-100vh');
                 $('.mobile-menu-show').css('margin-left','-100vh');
                 $('.title-name').css('display','block');
@@ -87,6 +88,7 @@ const showMenu =(event) =>{
             $('.close').css('display', 'block');
             $('.inline-menu').css('display', 'none');
             $('.mobile-menu-show').css('display','block');
+            $('.mobile-menu-back').css('display','block');
             $('.mobile-menu-back').css('margin-left','0');
             $('.mobile-menu-show').css('margin-left','0');
             $('.title-name').css('display','none');
